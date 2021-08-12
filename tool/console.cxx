7fe1849904ad6d0f8e8b848fcbdb716c3e05d5b6
Jad Altahan <xviyy@aol.com>
2021-08-11 18:27:24 -0700

    Normalize soft tabs

--- tool/console.cxx
+++ tool/console.cxx
@@ -9,43 +9,43 @@
 #include "console.h"
 
 Console::Console() {
-	m_pWi = new Screen;
-	if (!m_pWi)
-		m_pWi = nullptr;
+    m_pWi = new Screen;
+    if (!m_pWi)
+        m_pWi = nullptr;
 
-	m_pWi->handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	if (m_pWi->handle != nullptr)
-	{
-		if (!GetConsoleScreenBufferInfo(m_pWi->handle, &m_pWi->bufferInfo))
-			m_pWi->handle = nullptr;
-	}
+    m_pWi->handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (m_pWi->handle != nullptr)
+    {
+        if (!GetConsoleScreenBufferInfo(m_pWi->handle, &m_pWi->bufferInfo))
+            m_pWi->handle = nullptr;
+    }
 }
 
 void Console::SetTextColor(Color fg, Color bg)
 {
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	if (m_pWi == nullptr || !GetConsoleScreenBufferInfo(m_pWi->handle, &csbi))
-		return;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (m_pWi == nullptr || !GetConsoleScreenBufferInfo(m_pWi->handle, &csbi))
+        return;
 
-	WORD color = csbi.wAttributes;
-	if (fg != Console::Color::COLOR_NONE)
-		color = (color & 0xFFF0) | (int)fg;
+    WORD color = csbi.wAttributes;
+    if (fg != Console::Color::COLOR_NONE)
+        color = (color & 0xFFF0) | (int)fg;
 
-	if (bg != Console::Color::COLOR_NONE)
-		color = (color & 0xFF0F) | ((int)bg << 4);
+    if (bg != Console::Color::COLOR_NONE)
+        color = (color & 0xFF0F) | ((int)bg << 4);
 
-	if (m_pWi->handle != nullptr)
-		SetConsoleTextAttribute(m_pWi->handle, color);
+    if (m_pWi->handle != nullptr)
+        SetConsoleTextAttribute(m_pWi->handle, color);
 }
 
 void Console::ResetTextColor()
 {
-	if (m_pWi->handle != nullptr)
-		SetConsoleTextAttribute(m_pWi->handle, m_pWi->bufferInfo.wAttributes);
+    if (m_pWi->handle != nullptr)
+        SetConsoleTextAttribute(m_pWi->handle, m_pWi->bufferInfo.wAttributes);
 }
 
 Console::~Console()
 {
-	if (m_pWi)
-		delete m_pWi;
+    if (m_pWi)
+        delete m_pWi;
 }
